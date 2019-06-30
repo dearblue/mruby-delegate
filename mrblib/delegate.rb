de9556077c7a48a3ec1cf3f1b6d3aac8804a51a4
dearblue <dearblue@users.noreply.github.com>
2019-06-30 15:58:47 +0900

    コメントしてある無効なコードを削除

--- mrblib/delegate.rb
+++ mrblib/delegate.rb
@@ -1,33 +1,7 @@
 #!ruby
 
 class Delegator
-  #def initialize(obj)
-  #  __setobj__ obj
-
-  #  methods = obj.methods
-  #  specials = %i(__setobj__ __getobj__ method_missing)
-  #  methods -= specials
-
-  #  obj = nil # このスコープにおける元のオブジェクトへの参照を無効にする
-
-  #  singleton_class.instance_eval do
-  #    methods.each do |mid|
-  #      define_method(mid) do |*args, &block|
-  #        __getobj__.__send__(mid, *args, &block)
-  #      end
-  #    end
-  #  end
-  #end
-
   def method_missing(*args, &block)
     __getobj__.__send__(*args, &block)
   end
-
-  #def __setobj__(obj)
-  #  raise NotImplementedError, "need to define `__setobj__' method"
-  #end
-
-  #def __getobj__
-  #  raise NotImplementedError, "need to define `__getobj__' method"
-  #end
 end
